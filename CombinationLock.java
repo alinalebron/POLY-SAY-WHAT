a166582d367e690fee42e9a266616ec79fd9eb4d
Dante Elrik <dante@cloudshopper.com>
2015-03-25 22:04:01 -0400

    Rough Sketch

---
+++ CombinationLock.java
@@ -0,0 +1,27 @@
+public class CombinationLock {
+    private boolean open;
+    private int counter;
+
+    public CombinationLock(String first, String second, String third)
+    {
+        open = true;
+        counter = 3;
+    }
+
+    public void setPosition(String aPosition)
+    {
+    }
+
+    public void tryToOpen()
+    {
+    }
+
+    public boolean isOpen()
+    {
+    }
+
+    public void lock()
+    {
+    }
+
+}
